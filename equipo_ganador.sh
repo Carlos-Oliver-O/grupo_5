dcafe0803145ef8208a77dc666826467bece4ec7
Coke Stuyck <cokecancook@gmail.com>
2024-10-07 19:46:52 +0200

    Update equipo_ganador.sh

--- equipo_ganador.sh
+++ equipo_ganador.sh
@@ -9,4 +9,9 @@ echo "El primer parámetro es ${nombre1}"
 
 chmod +x equipo_ganador.sh
 
-./equipo_ganador.sh 
+if (( minute_stamp % 2 == 0 )); then
+  message = "Has ganado"
+else
+  message = "Has perdido"
+
+message >> ${nombre}/ganador.txt
